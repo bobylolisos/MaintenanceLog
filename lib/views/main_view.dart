7e3544283400c4049e1012b3b823f1967763b6cf
Marcus Bylsäter <marcus.bylsater@knowit.se>
2023-12-15 17:44:09 +0100

    Created empty views for tabbar

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:maintenance_log/resources/colors.dart';
+import 'package:maintenance_log/views/about_view.dart';
+import 'package:maintenance_log/views/home_view.dart';
+import 'package:maintenance_log/views/manage_view.dart';
 import 'package:maintenance_log/widgets/wave_clipper.dart';
 
 class MainView extends StatelessWidget {
-  const MainView({super.key});
+  MainView({super.key});
+
+  final ValueNotifier<int> _selectedMenuItemNotifier = ValueNotifier<int>(0);
 
   @override
   Widget build(BuildContext context) {
@@ -68,48 +73,80 @@ class MainView extends StatelessWidget {
                             ],
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
               ),
+              Expanded(
+                child: ValueListenableBuilder(
+                  valueListenable: _selectedMenuItemNotifier,
+                  builder: (BuildContext context, int value, Widget? child) {
+                    if (value == 2) {
+                      return AboutView();
+                    }
+                    if (value == 1) {
+                      return ManageView();
+                    }
+                    return HomeView();
+                  },
+                ),
+              ),
+              RotatedBox(
+                quarterTurns: 2,
+                child: ClipPath(
+                  clipper: WaveClipper(),
+                  child: Container(
+                    height: 30,
+                    color: colorBlue,
+                  ),
+                ),
+              )
             ],
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: colorBlue,
-          selectedItemColor: colorGold,
-          unselectedItemColor: colorGold.withOpacity(0.6),
-          items: [
-            BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: FaIcon(
-                    size: 18,
-                    FontAwesomeIcons.house,
-                  ),
-                ),
-                label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: FaIcon(
-                    size: 18,
-                    FontAwesomeIcons.gear,
-                  ),
-                ),
-                label: 'Manage'),
-            BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: FaIcon(
-                    size: 18,
-                    FontAwesomeIcons.receipt,
-                  ),
-                ),
-                label: 'About'),
-          ]),
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: _selectedMenuItemNotifier,
+        builder: (BuildContext context, int value, Widget? child) {
+          return BottomNavigationBar(
+              iconSize: 18,
+              selectedFontSize: 16,
+              currentIndex: _selectedMenuItemNotifier.value,
+              backgroundColor: colorBlue,
+              selectedItemColor: colorGold,
+              unselectedItemColor: colorGold.withOpacity(0.5),
+              onTap: (value) {
+                _selectedMenuItemNotifier.value = value;
+              },
+              items: [
+                BottomNavigationBarItem(
+                    icon: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FaIcon(
+                        FontAwesomeIcons.house,
+                      ),
+                    ),
+                    label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FaIcon(
+                        FontAwesomeIcons.gear,
+                      ),
+                    ),
+                    label: 'Manage'),
+                BottomNavigationBarItem(
+                    icon: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FaIcon(
+                        FontAwesomeIcons.receipt,
+                      ),
+                    ),
+                    label: 'About'),
+              ]);
+        },
+      ),
     );
   }
 }
---
+++ lib/views/manage_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ManageView extends StatelessWidget {
+  const ManageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Manage'),
+    );
+  }
+}
