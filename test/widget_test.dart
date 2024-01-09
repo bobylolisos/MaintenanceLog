4b1f86dc1a39f40da9b534537fc5b286dfc7395f
Marcus Bylsäter <marcus.bylsater@knowit.se>
2024-01-09 09:27:17 +0100

    Fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:maintenance_log/main.dart';
+import 'package:maintenance_log/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
